135ddb2c4a58a6546f5675ce8540b2c9cee71ad1
spncr <spencerwberry@gmail.com>
2019-04-17 23:10:28 -0700

    Articles Working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,46 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "'#{@article.title}' published!"
+
+    redirect_to root_path()
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "'#{@article.title}' updated!"
+
+    redirect_to root_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "'#{@article.title}' is gone forever!"
+
+
+    redirect_to root_path()
   end
 end
